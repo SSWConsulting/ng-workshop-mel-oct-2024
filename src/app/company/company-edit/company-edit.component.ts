95a4c61d18dda6ae95b16c804f397fe8b6717cbb
Matt Wicks <matt.wicks@gmail.com>
2024-10-14 15:49:50 +1100

    companyedit - added reactive form to ui

--- src/app/company/company-edit/company-edit.component.ts
+++ src/app/company/company-edit/company-edit.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
+import { Company } from '../company';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'fbc-company-edit',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './company-edit.component.html',
-  styleUrl: './company-edit.component.scss'
+  styleUrl: './company-edit.component.scss',
 })
 export class CompanyEditComponent {
+  companyForm!: FormGroup;
 
+  ngOnInit(): void {
+    this.companyForm = new FormGroup({
+      name: new FormControl('', [Validators.required, Validators.minLength(3)]),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      phone: new FormControl(''),
+    });
+  }
+
+  saveCompany() {
+    this.companyForm.markAllAsTouched();
+  }
 }
