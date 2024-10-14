8665ab86b89d9e01567b9781dbf0a4cfc12a0a51
Matt Wicks <matt.wicks@gmail.com>
2024-10-14 16:46:43 +1100

    get ready for editing company

--- src/app/company/company-edit/company-edit.component.ts
+++ src/app/company/company-edit/company-edit.component.ts
@@ -9,7 +9,7 @@ import {
 } from '@angular/forms';
 import { Company } from '../company';
 import { CompanyService } from '../company.service';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 type CompanyEditFormGroup = {
   [K in keyof Company]: FormControl<Company[K] | null>;
@@ -23,31 +23,47 @@ type CompanyEditFormGroup = {
   styleUrl: './company-edit.component.scss',
 })
 export class CompanyEditComponent {
+  isNewCompany = false;
   companyForm!: FormGroup<CompanyEditFormGroup>;
 
   constructor(
+    private readonly activatedRoute: ActivatedRoute,
     private readonly fb: FormBuilder,
     private readonly router: Router,
     private readonly companyService: CompanyService,
   ) {}
 
   ngOnInit(): void {
+    const companyId = +this.activatedRoute.snapshot.params['id'];
+    this.isNewCompany = !(companyId > 0);
+
     this.companyForm = this.fb.group<CompanyEditFormGroup>({
       id: this.fb.control(0),
       name: this.fb.control('', [Validators.required, Validators.minLength(3)]),
       email: this.fb.control('', [Validators.required, Validators.email]),
       phone: this.fb.control(''),
     });
+
+    if (!this.isNewCompany) {
+      // TODO: load company and patch form
+    }
   }
 
   saveCompany() {
     this.companyForm.markAllAsTouched();
 
-    if (this.companyForm.valid) {
-      const company = this.companyForm.value as Company;
+    if (!this.companyForm.valid) {
+      return;
+    }
+
+    const company = this.companyForm.value as Company;
+
+    if (this.isNewCompany) {
       this.companyService.addCompany(company).subscribe((_) => {
         this.router.navigate(['/company/list']);
       });
+    } else {
+      throw new Error('Not implemented');
     }
   }
 }
